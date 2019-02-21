7315c8df69639f9540ee68c94a120c7ae026ad53
zeyofu <fxylyly@yahoo.com>
2019-02-21 02:17:20 -0600

    save

--- graphsage-simple/graphsage/encoders.py
+++ graphsage-simple/graphsage/encoders.py
@@ -3,15 +3,17 @@ import torch.nn as nn
 from torch.nn import init
 import torch.nn.functional as F
 
+
 class Encoder(nn.Module):
     """
     Encodes a node's using 'convolutional' GraphSage approach
     """
-    def __init__(self, features, feature_dim, 
-            embed_dim, adj_lists, aggregator,
-            num_sample=10,
-            base_model=None, gcn=False, cuda=False, 
-            feature_transform=False): 
+
+    def __init__(self, features, feature_dim,
+                 embed_dim, adj_lists, aggregator,
+                 num_sample=10,
+                 base_model=None, gcn=False, cuda=False,
+                 feature_transform=False):
         super(Encoder, self).__init__()
 
         self.features = features
@@ -19,7 +21,7 @@ class Encoder(nn.Module):
         self.adj_lists = adj_lists
         self.aggregator = aggregator
         self.num_sample = num_sample
-        if base_model != None:
+        if base_model is not None:
             self.base_model = base_model
 
         self.gcn = gcn
@@ -27,7 +29,7 @@ class Encoder(nn.Module):
         self.cuda = cuda
         self.aggregator.cuda = cuda
         self.weight = nn.Parameter(
-                torch.FloatTensor(embed_dim, self.feat_dim if self.gcn else 2 * self.feat_dim))
+            torch.FloatTensor(embed_dim, self.feat_dim if self.gcn else 2 * self.feat_dim))
         init.xavier_uniform(self.weight)
 
     def forward(self, nodes):
@@ -36,8 +38,8 @@ class Encoder(nn.Module):
 
         nodes     -- list of nodes
         """
-        neigh_feats = self.aggregator.forward(nodes, [self.adj_lists[int(node)] for node in nodes], 
-                self.num_sample)
+        neigh_feats = self.aggregator.forward(nodes, [self.adj_lists[int(node)] for node in nodes],
+                                              self.num_sample)
         if not self.gcn:
             if self.cuda:
                 self_feats = self.features(torch.LongTensor(nodes).cuda())
